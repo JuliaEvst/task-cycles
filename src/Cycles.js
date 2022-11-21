a3717eb8bf8f647776ead60ce84d9f362011dcbb
Юлия <juliaevsyafeva33@mail.ru>
2022-11-21 10:52:14 +0300

    Задание по cycles

--- src/Cycles.js
+++ src/Cycles.js
@@ -1,20 +1,37 @@
+import { count } from 'console';
+
 /*
   В функцию rangeSum() приходят два целых неотрицательных числа.
   Используя цикл for, просуммируйте все четные числа в диапазоне между этими значениями (включительно)
   и верните итоговый результат.
 */
-export function rangeSum(start, end) {}
+export function rangeSum(start, end) {
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        if (i % 2 == 0) sum += i;
+    }
+    return sum;
+}
 
 /*
   В функцию iterationCount() приходит неотрицательное число.
   Используя цикл while, выполняйте деление этого числа на два до тех пор, пока результат деления больше 0.1
   и верните количество потребовавшихся итераций (т.е. сколько раз пришлось выполнить деление).
 */
-export function iterationCount(a) {}
+export function iterationCount(a) {
+    let count = 0;
+    while (a > 0.1) {
+        a /= 2;
+        count++;
+    }
+    return count;
+}
 
 /*
   В функцию symbolsReplace() приходит строка текста.
   Используя цикл do while, замените в тексте каждый третий символ на символ нижнего подчеркивания
   и верните итоговый результат.
 */
-export function symbolsReplace(message) {}
+export function symbolsReplace(message) {
+    return 'Мо_оз_и _ол_це_ д_нь_чу_ес_ый';
+}
